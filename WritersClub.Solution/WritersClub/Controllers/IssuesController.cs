f09d743d9e1aea8a881ea4b36b5c995fdd310f47
Krista Rutz <rutzkri000@gmail.com>
2020-03-25 14:40:01 -0700

    Modify files to show Issue info
    Co-authored-by: Steven Fleming <12flemings@gmail.com>

---
+++ WritersClub.Solution/WritersClub/Controllers/IssuesController.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+using WritersClub.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace WritersClub.Controllers
+{
+  public class IssuesController : Controller
+  {
+
+    private readonly WritersClubContext _db;
+
+    public IssuesController(WritersClubContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Issue> model = _db.Issues.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Issue issue)
+    {
+      _db.Issues.Add(issue);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    //     public ActionResult Details(int id)
+    //     {
+    //       Issue thisIssue = _db.Issues
+    //         .Include(issue => issue.Items)
+    //         .ThenInclude(join => join.Item)
+    //         .FirstOrDefault(issue => issue.IssueId == id);    
+    //       // thisIssue.Items = _db.Items.Where(item => item.IssueId == id).ToList();
+    //       return View(thisIssue);
+    //     }
+
+    //     public ActionResult Edit(int id)
+    //     {
+    //       var thisIssue = _db.Issues.FirstOrDefault(issues => issues.IssueId == id);
+    //       return View(thisIssue);
+    //     }
+
+    //     [HttpPost]
+    //     public ActionResult Edit(Issue issue)
+    //     {
+    //       _db.Entry(issue).State = EntityState.Modified;
+    //       _db.SaveChanges();
+    //       return RedirectToAction("Index");
+    //     }
+
+    //     public ActionResult Delete(int id)
+    //     {
+    //       var thisIssue = _db.Issues.FirstOrDefault(issues => issues.IssueId == id);
+    //       return View(thisIssue);
+    //     }
+
+    //     [HttpPost, ActionName("Delete")]
+    //     public ActionResult DeleteConfirmed(int id)
+    //     {
+    //       var thisIssue = _db.Issues.FirstOrDefault(issues => issues.IssueId == id);
+    //       _db.Issues.Remove(thisIssue);
+    //       _db.SaveChanges();
+    //       return RedirectToAction("Index");
+    //     }
+  }
+}
